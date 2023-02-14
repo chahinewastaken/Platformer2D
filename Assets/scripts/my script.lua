bc21b947d160596eed21ce3cce1f2d2db6b34ea3
chahinewastaken <108815753+chahinewastaken@users.noreply.github.com>
2023-02-14 16:09:27 +0100

    update yes

---
+++ Assets/scripts/my script.lua
@@ -0,0 +1,6 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("cwtScript", "BloodTheme")
+
+--MAIN
+local MAIN = Tab:NewSection("Main")
+local MainSection = Main:NewSection("Main")
